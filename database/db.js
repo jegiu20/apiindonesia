1990a5840e2faa582b3cd8ef9612366df7eac5b8
jegiu <111652503+jegiu20@users.noreply.github.com>
2023-02-17 00:27:47 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://alphabot-api.herokuapp.com';
+var url = 'https://api-adr.cyclic.app';
 try {
-if(url == 'https://alphabot-api.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api-adr.cyclic.app') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
